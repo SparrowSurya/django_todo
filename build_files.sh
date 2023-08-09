d57075f673a972f4dc18a1539018845b79cb945a
SparrowSurya <thakur.suryakant.0406@gmail.com>
2023-08-10 00:14:26 +0530

    added remote database & removed Pipfile

--- build_files.sh
+++ build_files.sh
@@ -5,4 +5,5 @@ python3.9 -m pip install -r requirements.txt
 echo "collecting staticfiles ..."
 python3.9 manage.py collectstatic --noinput --clear
 
-# python3.9 manage.py migrate --noinput
+echo "migrate ..."
+python3.9 manage.py migrate --noinput
--- main/settings.py
+++ main/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +79,12 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ['DB_ENGINE'],
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
     }
 }
 
