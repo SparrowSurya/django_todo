d88385088eba6c45057db893ba78083c360b8505
SparrowSurya <thakur.suryakant.0406@gmail.com>
2023-08-10 00:00:01 +0530

    modified build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,8 @@
-python -m pip install -r requirements.txt
-python manage.py collectstatic
-# python manage.py migrate
+
+echo "Installing requirements ..."
+python3.9 -m pip install -r requirements.txt
+
+echo "collecting staticfiles ..."
+python3.9 manage.py collectstatic --noinput --clear
+
+# python3.9 manage.py migrate --noinput
